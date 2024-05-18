f8a699233f965898d2d12d712f57da05d332a43b
Subx1s0o <nazarsabardin@gmail.com>
2024-05-18 04:53:14 +0300

    push

--- main.js
+++ main.js
@@ -11,43 +11,43 @@
 //     console.log(data);
 //   })
 //   .catch((error) => console.log(error));
-import filmsAPi from "./js/fetch";
-import renderCards from "./js/render";
+import filmsAPi from './src/js/fetch.js';
+import renderCards from './src/js/render.js';
 
-const totalSearch = document.querySelector(".totalSearch span");
-const input = document.querySelector(".input");
-const list = document.querySelector(".list");
-const form = document.querySelector(".form");
-const loader = document.querySelector(".loader");
-const errormsg = document.querySelector(".errormsg");
-totalSearch.textContent = "0";
+const totalSearch = document.querySelector('.totalSearch span');
+const input = document.querySelector('.input');
+const list = document.querySelector('.list');
+const form = document.querySelector('.form');
+const loader = document.querySelector('.loader');
+const errormsg = document.querySelector('.errormsg');
+totalSearch.textContent = '0';
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   const search = input.value.trim();
-  list.innerHTML = "";
-  input.value = "";
-  loader.style.visibility = "visible";
-  errormsg.style.display = "none";
+  list.innerHTML = '';
+  input.value = '';
+  loader.style.visibility = 'visible';
+  errormsg.style.display = 'none';
   filmsAPi(search)
-    .then((data) => {
-      if (search !== "" && data.Response === "True") {
-        loader.style.visibility = "hidden";
+    .then(data => {
+      if (search !== '' && data.Response === 'True') {
+        loader.style.visibility = 'hidden';
         totalSearch.textContent = data.totalResults;
 
         renderCards(data, list);
       } else {
-        totalSearch.textContent = "0 ";
-        errormsg.style.display = "block";
-        errormsg.textContent = "We don`t find the films :(";
+        totalSearch.textContent = '0 ';
+        errormsg.style.display = 'block';
+        errormsg.textContent = 'We don`t find the films :(';
 
-        loader.style.visibility = "hidden";
+        loader.style.visibility = 'hidden';
       }
     })
-    .catch((error) => {
-      loader.style.visibility = "hidden";
-      console.log("Cant take response", error);
-      errormsg.style.display = "block";
-      errormsg.textContent = "Sorry, check your internet connection! :(";
+    .catch(error => {
+      loader.style.visibility = 'hidden';
+      console.log('Cant take response', error);
+      errormsg.style.display = 'block';
+      errormsg.textContent = 'Sorry, check your internet connection! :(';
     });
 });
